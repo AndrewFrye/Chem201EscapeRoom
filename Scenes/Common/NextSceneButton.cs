92b5eedf435419fa596403f867b2571ec3ea122c
Andrew Frye <dewdew72805@gmail.com>
2023-11-02 12:06:03 -0400

    Added nextscenebutton.cs to source as it was missed in a previous commit

---
+++ Scenes/Common/NextSceneButton.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public class NextSceneButton : Button
+{
+    [Export]
+    public string nextSceneString = "";
+
+    public override void _Pressed()
+    {
+        GetTree().ChangeScene(nextSceneString);
+    }
+}
