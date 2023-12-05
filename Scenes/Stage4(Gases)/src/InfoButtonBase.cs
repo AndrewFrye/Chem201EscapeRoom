2f2ed8fb83a212ea5dd9f6b08ee001b57f3a7ddc
Andrew Frye <dewdew72805@gmail.com>
2023-12-05 17:17:48 -0500

    Finished logic for stage 4

--- Scenes/Stage4(Gases)/src/InfoButtonBase.cs
+++ Scenes/Stage4(Gases)/src/InfoButtonBase.cs
@@ -4,33 +4,12 @@ namespace EscapeRoom.Scenes.Stage4_Gases_.src
 {
     public class InfoButtonBase : Button
     {
-        [Export] public string InfoLabelPath { get; set; }
-        [Export] public string LabelText { get; set; }
-        [Export] public int InfoId { get; set; }
-        
-        private InfoLabel InfoLabel;
-        
-        public override void _Ready()
-        {
-            InfoLabel = GetNode<InfoLabel>(InfoLabelPath);
-        }
+        [Export] public string PopupPath { get; set; }
 
         public override void _Pressed()
         {
-            if (InfoLabel.LabelVisible && InfoLabel.InfoId == InfoId)
-            {
-                
-                    InfoLabel.LabelVisible = false;
-                    InfoLabel.InfoId = -1;
-                
-            }
-            else
-            {
-                InfoLabel.LabelVisible = false;
-                InfoLabel.InfoId = InfoId;
-
-                InfoLabel.Text = LabelText;
-            }
+            var popup = GetNode<WindowDialog>(PopupPath);
+            popup.PopupCenteredClamped();
         }
     }
 }
