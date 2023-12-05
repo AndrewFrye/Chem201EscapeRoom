afcbf5d2f758d58065885158fbe247c8ce2d76a8
Andrew Frye <dewdew72805@gmail.com>
2023-12-04 19:20:12 -0500

    Added new text formatting

---
+++ Resources/Fonts/RichTextEffects/Src/Subscript.gd
@@ -0,0 +1,8 @@
+﻿tool
+extends RichTextEffect
+
+
+var bbcode = "subscript"
+func _process_custom_fx(char_fx : CharFXTransform):
+    char_fx.offset = Vector2(0, char_fx.env.offset_y if char_fx.env.has("offset_y") else 10)
+    return true
---
+++ Resources/Fonts/RichTextEffects/Src/Superscript.gd
@@ -0,0 +1,9 @@
+tool
+extends RichTextEffect
+
+
+var bbcode = "superscript"
+func _process_custom_fx(char_fx : CharFXTransform):
+    char_fx.offset = Vector2(0, char_fx.env.offset_y if char_fx.env.has("offset_y") else -10)
+    return true
+    
