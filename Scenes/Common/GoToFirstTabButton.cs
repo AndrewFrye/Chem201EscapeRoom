e6b234615d4386b907420f501059bb1abd423420
Andrew Frye <dewdew72805@gmail.com>
2023-12-05 22:41:43 -0500

    Changed stage 6

---
+++ Scenes/Common/GoToFirstTabButton.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+namespace EscapeRoom.Scenes.Common
+{
+    public class GoToFirstTabButton : Button
+    {
+        [Export] private string TabContainerPath { get; set; }
+
+        public override void _Pressed()
+        {
+            GetNode<TabContainer>("%SubQuestions").CurrentTab = 0;
+            GetParent<WindowDialog>().Hide();
+        }
+    }
+}
